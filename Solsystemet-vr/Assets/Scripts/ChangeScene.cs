5e0486b9f19df3b4dc6a53cc3a87a46cc2f4a921
Maja Blom Wennerwald Nielsen <70579734+Gajzii@users.noreply.github.com>
2023-04-10 15:33:28 +0200

    d

--- Solsystemet-vr/Assets/Scripts/ChangeScene.cs
+++ Solsystemet-vr/Assets/Scripts/ChangeScene.cs
@@ -7,12 +7,18 @@ public class ChangeScene : MonoBehaviour
 {
 
     public GameObject canvas;
+    public AudioSource clickSound;
     
     public void HideUI(){
+        PlayClickSound();
         canvas.SetActive(false);
     }
     public void MoveToScene(int sceneID)
     {
+        PlayClickSound();
         SceneManager.LoadScene(sceneID);
     }
+    public void PlayClickSound(){
+        clickSound.Play();
+        }
 }

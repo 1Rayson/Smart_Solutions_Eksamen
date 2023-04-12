edeabb9e99539331e3cf79d6c51c99189f225104
MarBProduction <MarBProduction@users.noreply.github.com>
2023-04-12 11:12:58 +0200

    does this work 1

--- Solsystemet-vr/Assets/Scripts/PlanetDistance.cs
+++ Solsystemet-vr/Assets/Scripts/PlanetDistance.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class PlanetDistance : MonoBehaviour
 {
-
     public GameManager gameManager;
-    Vector3 correctDistance;
     public float startYRotation = 73f;
     public float tolerance = 1f;
+
     private bool rightPlace = false;
+    Vector3 correctDistance;
+    Vector3 startPosition;
+    Vector3 startRotation;
+    Vector3 newRotation;
     // Start is called before the first frame update
     void Start()
     {
         correctDistance = transform.localEulerAngles;
         transform.localEulerAngles = new Vector3(0, startYRotation, 0);
+
+        startPosition = transform.position;
+        startRotation = transform.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {       
         if(
             transform.localEulerAngles.y > correctDistance.y - tolerance && 
             transform.localEulerAngles.y < correctDistance.y + tolerance
@@ -31,5 +37,14 @@ public class PlanetDistance : MonoBehaviour
                 rightPlace = true;
             }
         }
+
+        float newRotationY = transform.eulerAngles.y;
+
+        newRotation = new Vector3(startRotation.x, newRotationY, startRotation.z);
+    }
+
+    void LateUpdate(){
+        transform.position = startPosition;
+        transform.eulerAngles = newRotation;
     }
 }
